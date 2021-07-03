baedc5dd86e708c425abec695d9f67ab9c616a47
incrypto <vpkrishnanand@gmail.com>
2021-07-03 11:08:57 +0530

    set set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import "./styles/App.css";
 
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-     
-       <h2>ONE Canvas</h2>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>One Canvas 5</h2>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
